8501f246ec9ad298b4c81d4bff802b56a33525eb
qkstngus000 <bans@wwu.edu>
2022-04-27 20:53:43 -0700

    Starting on Skeleton Code

---
+++ implementaion/DeadWood.java
@@ -0,0 +1,36 @@
+package implementaion;
+
+/*
+* Class: DeadWood
+* Description: This class is responsible for ~~~
+*
+*
+*
+*
+*/
+public class DeadWood {
+    private int day;    // Take cares of days that players play depending on their number
+    private int numPlayer;  // Input of number of players
+    private Player[] listPlayer;    // List of players to keep track of turn
+    private Board board;    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+
+    /* 
+    * Function: setEnv()
+    * Parameter: None
+    *   Param1: Where did it came from why we need it
+    *   param2:  
+    * 
+    * Return: void
+    *   This return type is going to be used int ~~~ class method for ~~~ purposes
+    */
+    public void setEnv() {
+
+    }
+
+    
+}
