8199b812da78b6c039f3788287f7757fad00898a
qkstngus000 <bans@wwu.edu>
2022-05-02 06:51:08 -0700

    Player skeleton Code finished

--- implementation/Player.java
+++ implementation/Player.java
@@ -9,8 +9,125 @@ package implementation;
  * 
  */
 public class Player {
+    private String name; // Holds player name
+    private Room room; // Holds current status of player location
+    private int rank; // Holds rank of current player
+    private int dollar; // Holds how many dollars player has
+    private int credit; // Holds how many credit player has
+    private int token; // Holds how many rehearse token player has
+    private Role role; // Holds current role of player. If not, null
+    private int toalScore;
+
+    /*
+     * Constructor Player
+     * Parameter:
+     * String name: Player input of name on their character representation
+     * Trailer startRoom: Trailer room where players would start from
+     * Description:
+     * Initialize the player object to be created which would hold player
+     * info, and update the player info as games progress.
+     */
     public Player(String name, Trailer startRoom) {
         // TO DO
+    }
 
+    /*
+     * Function: playerTurn
+     * Parameter:
+     * None
+     * Description:
+     * Interact with DeadWood manager and keep track of the turn
+     * as there are going to be many Player objects.
+     */
+    public void playerTurn() {
+        // TO DO
+    }
+
+    /*
+     * Function: calcScore
+     * Parameter:
+     * None
+     * Description:
+     * At the end of the game, this function will grab player's dollar,
+     * credit, and rank to calculate the player score.
+     */
+    public void calcScore() {
+        // TO DO
+    }
+
+    /*
+     * Function: takeRole
+     * Parameter:
+     * Role role: the role that player is wanting to take
+     * 
+     * Description:
+     * If player choose to take role, this function will change the player
+     * status and make sure the role is available for player by comparing it
+     * to player's current rank status.
+     */
+    public boolean takeRole(Role role) {
+        // TO DO
+        return false;
+    }
+
+    /*
+     * Function: act
+     * Parameter:
+     * Dice dice: dice object that player would role for act
+     * 
+     * Description:
+     * If player wants to act on player's role, function calls the dice obejct
+     * to receive the rolled number and compare with budget.
+     */
+    public void act(Dice dice) {
+        // TO DO
+    }
+
+    /*
+     * Function: rehearse
+     * Parameter:
+     * None
+     * 
+     * Description:
+     * If player wish to rehearse rather than act, rehearse function will increment
+     * player token until the number reaches budget - 1 for the scene.
+     * 
+     * Return: boolean
+     * If player wish to rehearse while token is already budget - 1, return false
+     * to notify player it is wrong move.
+     */
+    public boolean rehearse() {
+        // TO DO
+        return false;
+    }
+
+    /*
+     * Function: removeRole
+     * Parameter:
+     * None
+     * 
+     * Description:
+     * After clearing out the scene, remove the role from player
+     * Token gets updated to be 0 as well.
+     */
+    public void removeRole() {
+        // TO DO
+    }
+
+    /*
+     * Function: move
+     * Parameter:
+     * None
+     * 
+     * Description:
+     * Allows player to move to different rooms.
+     * 
+     * Return: boolean
+     * If player wants to move to adjecent room, return true.
+     * Otherwise, return false.
+     */
+    public boolean move() {
+        // TO DO
+        return false;
     }
 }
